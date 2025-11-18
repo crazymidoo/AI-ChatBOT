589573d1d661c31ae1a0dac86060a2bd9f5c4ddf
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-11-18 09:49:27 +0000

    Load Training Data

--- Project/training.py
+++ Project/training.py
@@ -11,3 +11,21 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('json/data.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
