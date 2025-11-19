6b769e367c859b229bc41518538e7195deb82eaf
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-11-19 19:23:13 +0000

    Modifiche chatbot py

--- Project/chatbot.py
+++ Project/chatbot.py
@@ -11,22 +11,64 @@ lemmatizer = WordNetLemmatizer()
 with open('json/data.json', 'r') as f:
     intents = json.load(f)
 
+#Carichiamo le parole e le classi create nel training
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
+
+#Carichiamo il modello addestrato
 model = load_model('chatbot_model.keras')
 
 #Funzione per pulire le frasi
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
 #"Bag of Words", converte una frase in una lista di 0 e 1 che indica se c'è la parola o no
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
-    bag=[0] * len(words)
+    bag = [0] * len(words)
     for w in sentence_words:
-        for i, word in  enumerate(words):
+        for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
+#Predice quale intent è più probabile dato un input
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]), verbose=0)[0]
+    ERROR_THRESHOLD = 0.25
+
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    #ordina dal più probabile al meno probabile
+    results.sort(key=lambda x: x[1], reverse=True)
+
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+
+    if len(return_list) == 0:
+        return [{'intent': 'noanswer', 'probability': '1'}]
+
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Neural è pronto a parlare! (digita 'quit' per uscire)")
+while True:
+    message = input("Tu: ")
+    if message.lower() == "quit":
+        break
+
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print("Neural:", res)
